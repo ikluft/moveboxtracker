eab4b42085bdc69181c67d9ec8040c0eda45f8ef
Ian Kluft <ko6yq.dev+github@gmail.com>
2023-01-31 23:59:18 -0800

    Python: more CLI start

---
+++ cli/src/moveboxtracker/cli.py
@@ -0,0 +1,8 @@
+import argparse
+from pathlib import Path
+from xdg import BaseDirectory
+
+parser = argparse.ArgumentParser(
+        prog="moveboxtracker",
+        description="moving box database manager and label generator"
+        )
