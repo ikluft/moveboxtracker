f992748959f26ce47792c079bd93c21c2e51114a
Ian Kluft <ko6yq.dev+github@gmail.com>
2023-02-01 18:58:52 -0800

    Python: get main to print help and version; code cleanup

---
+++ cli/src/moveboxtracker/__main__.py
@@ -0,0 +1,5 @@
+"""run command-line interface for moveboxtracker"""
+from moveboxtracker import cli
+
+# start command-line
+cli.run()
--- cli/src/moveboxtracker/cli.py
+++ cli/src/moveboxtracker/cli.py
@@ -28,6 +28,7 @@ All of the databasea access operations take the following ("CRUD") arguments:
     --delete | --del | -d   id
 """
 import argparse
+from importlib.metadata import (version, PackageNotFoundError)
 from pathlib import Path
 from xdg import BaseDirectory
 
@@ -35,67 +36,76 @@ from xdg import BaseDirectory
 data_home = BaseDirectory.xdg_data_home
 
 
-def do_init():
+def _get_version():
+    """display version"""
+    try:
+        ver = str(version('moveboxtracker'))
+    except PackageNotFoundError:
+        ver = "development environment - version not available"
+    return ver
+
+
+def _do_init():
     """initialize new moving box database"""
     raise Exception("not implemented")  # TODO
 
 
-def do_label():
+def _do_label():
     """print label(s) for specified box ids"""
     raise Exception("not implemented")  # TODO
 
 
-def do_merge():
+def _do_merge():
     """merge in an external SQLite database file, from another device"""
     raise Exception("not implemented")  # TODO
 
 
-def do_log():
+def _do_log():
     """view log"""
     raise Exception("not implemented")  # TODO
 
 
-def do_db_batch():
+def _do_db_batch():
     """database access: batch/group of moving boxes"""
     raise Exception("not implemented")  # TODO
 
 
-def do_db_box():
+def _do_db_box():
     """database access: moving box including label info"""
     raise Exception("not implemented")  # TODO
 
 
-def do_db_item():
+def _do_db_item():
     """database access: item inside a box"""
     raise Exception("not implemented")  # TODO
 
 
-def do_db_location():
+def _do_db_location():
     """database access: location where boxes may be"""
     raise Exception("not implemented")  # TODO
 
 
-def do_db_log():
+def _do_db_log():
     """database access: log of data update events"""
     raise Exception("not implemented")  # TODO
 
 
-def do_db_project():
+def _do_db_project():
     """database access: overall move project info"""
     raise Exception("not implemented")  # TODO
 
 
-def do_db_room():
+def _do_db_room():
     """database access: room at origin & destination"""
     raise Exception("not implemented")  # TODO
 
 
-def do_db_scan():
+def _do_db_scan():
     """database access: box scan event on move to new location"""
     raise Exception("not implemented")  # TODO
 
 
-def do_db_user():
+def _do_db_user():
     """database access: user who owns database or performs a box scan"""
     raise Exception("not implemented")  # TODO
 
@@ -108,6 +118,7 @@ def run():
         prog="moveboxtracker",
         description="moving box database manager and label generator",
     )
+    top_parser.add_argument("--version", action="version", version=_get_version())
 
     # define subparsers for high-level operations
     subparsers = top_parser.add_subparsers(help="sub-command help")
@@ -117,25 +128,26 @@ def run():
     parser_init.add_argument("--primary_user", "--user")
     parser_init.add_argument("--title")
     parser_init.add_argument("--found_contact", "--found", "--contact")
-    parser_init.set_defaults(func=do_init)
+    parser_init.set_defaults(func=_do_init)
 
     parser_label = subparsers.add_parser(
         "label", help="print label(s) for specified box ids"
     )
     parser_label.add_argument("box_id", nargs="*", metavar="ID", type=int)
-    parser_label.set_defaults(func=do_label)
+    parser_label.set_defaults(func=_do_label)
 
     parser_merge = subparsers.add_parser(
         "merge", help="merge in an external SQLite database file, from another device"
     )
     parser_merge.add_argument("db_file", metavar="DB", help="database file")
-    parser_merge.set_defaults(func=do_merge)
+    parser_merge.set_defaults(func=_do_merge)
 
     parser_log = subparsers.add_parser("log", help="view log")
-    parser_log.set_defaults(func=do_log)
+    parser_log.set_defaults(func=_do_log)
 
     # define subparsers for low-level database access
-    subparsers_db = subparsers.add_subparsers(help="low-level db sub-command help")
+    parser_db = subparsers.add_parser("db", help="low-level database access subcommands")
+    subparsers_db = parser_db.add_subparsers(help="low-level db sub-command help")
 
     # parser_db_parent contains template for common parameters in all the db subparsers
     parser_db_parent = argparse.ArgumentParser(add_help=False)
@@ -147,50 +159,50 @@ def run():
     parser_db_batch = subparsers_db.add_parser(
         "batch", help="batch/group of moving boxes", parents=[parser_db_parent]
     )
-    parser_db_batch.set_defaults(func=do_db_batch)
+    parser_db_batch.set_defaults(func=_do_db_batch)
 
     parser_db_box = subparsers_db.add_parser(
         "box", help="moving box including label info", parents=[parser_db_parent]
     )
-    parser_db_box.set_defaults(func=do_db_box)
+    parser_db_box.set_defaults(func=_do_db_box)
 
     parser_db_item = subparsers_db.add_parser(
         "item", help="item inside a box", parents=[parser_db_parent]
     )
-    parser_db_item.set_defaults(func=do_db_item)
+    parser_db_item.set_defaults(func=_do_db_item)
 
     parser_db_location = subparsers_db.add_parser(
         "location", help="location where boxes may be", parents=[parser_db_parent]
     )
-    parser_db_location.set_defaults(func=do_db_location)
+    parser_db_location.set_defaults(func=_do_db_location)
 
     parser_db_log = subparsers_db.add_parser(
         "log", help="log of data update events", parents=[parser_db_parent]
     )
-    parser_db_log.set_defaults(func=do_db_log)
+    parser_db_log.set_defaults(func=_do_db_log)
 
     parser_db_project = subparsers_db.add_parser(
         "project", help="overall move project info", parents=[parser_db_parent]
     )
-    parser_db_project.set_defaults(func=do_db_project)
+    parser_db_project.set_defaults(func=_do_db_project)
 
     parser_db_room = subparsers_db.add_parser(
         "room", help="room at origin & destination", parents=[parser_db_parent]
     )
-    parser_db_room.set_defaults(func=do_db_room)
+    parser_db_room.set_defaults(func=_do_db_room)
 
     parser_db_scan = subparsers_db.add_parser(
         "scan", help="box scan event on move to new location", parents=[parser_db_parent]
     )
-    parser_db_scan.set_defaults(func=do_db_scan)
+    parser_db_scan.set_defaults(func=_do_db_scan)
 
     parser_db_user = subparsers_db.add_parser(
         "user", help="user who owns database or performs a box scan", parents=[parser_db_parent]
     )
-    parser_db_user.set_defaults(func=do_db_user)
+    parser_db_user.set_defaults(func=_do_db_user)
 
     # parse arguments and run subcommand functions
-    args = top_parser.parse_args()
-    if args.func is None:
-        raise Exception("no command was specified")
-    args.func(args)
+    args = vars(top_parser.parse_args())
+    if "func" not in args:
+        top_parser.error("no command was specified")
+    args["func"](args)
