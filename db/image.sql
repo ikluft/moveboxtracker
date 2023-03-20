b494fc7ddafa553931fa6a7bdcb2b0c39113a7ed
Ian Kluft <ko6yq.dev+github@gmail.com>
2023-03-19 23:52:18 -0700

    begin switch from image blobs to files outside database for maintainability

--- db/image.sql
+++ db/image.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS image (
   id INTEGER PRIMARY KEY NOT NULL,
-  imageblob blob NOT NULL,
+  image_file text UNIQUE NOT NULL,
   crc32 integer UNIQUE NOT NULL,
   mimetype text NOT NULL,
   description text,
